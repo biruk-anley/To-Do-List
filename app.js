0ec4a45f0d4e59cf405805186fa6c6affe510fe7
biruk_anley <birukanley2022@gmail.com>
2022-12-15 12:13:45 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('Task Manager App')
+const express = require('express')
+
+const app = express()
+
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
+// routes
+
+//app.get(api/v1/tasks)         - get all the tasks
+//app.post(api/v1/tasks)        - create a new task
+//app.get(api/v1/taks/:id)      - get a single task
+//app.path(api/v1.tasks/:id)    - update a task
+//app.delete(api/v1/tasks/:id)  -delete task
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+
+const port = 3000
+app.listen(port, console.log(`server is listening on port ${port}`))
