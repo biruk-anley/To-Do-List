a187638dd53bfb8380a4571a0eecbdc7aecfc18b
biruk_anley <birukanley2022@gmail.com>
2022-12-16 15:35:04 +0300

    udpating task.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,10 +3,9 @@ const express = require('express');
 //The express.Router() function is used to create a new router object. This function is used when you want to create a new router object in your program to handle requests. 
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-    console.log('here I am')
-    res.send('all items will be renderd here')
-    
-})
+const{getAllTasks,createTask,deleteTask,getTask, updateTask} = require('../controllers/tasks')
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
  
 module.exports = router;
+
