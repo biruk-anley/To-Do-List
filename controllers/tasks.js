f7b9e81dd342dac775f4a4084947d26443bc505b
biruk_anley <birukanley2022@gmail.com>
2022-12-17 23:13:57 +0300

    creating a model for database

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,13 @@
 //Controllers are responsible for handling incoming requests and returning responses to the client.A controller's purpose is to receive specific requests for the application.
 
+const Task = require('../model/Task')
 const getAllTasks = (req, res) => {
     res.send('Geting Items')
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
 }
 const getTask = (req, res) => {
     res.json({id:req.params.id})
