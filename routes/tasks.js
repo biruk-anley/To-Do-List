4a9f3d2483bd9bf156123b17bdc179500283060e
biruk_anley <birukanley2022@gmail.com>
2022-12-19 16:56:28 +0300

    update task

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
-//The express.Router() function is used to create a new router object. This function is used when you want to create a new router object in your program to handle requests. 
+//The express.Router() function is used to create a new router object. This function is used when you want to create a new router object in your program to handle requests.
+
+// I have used patch to update because I want to update or use partial update
+
+// partial : it will update what we want eg name only
+// put will replace all file all other files will be updated
 const router = express.Router();
 
 const{getAllTasks,createTask,deleteTask,getTask, updateTask} = require('../controllers/tasks')
